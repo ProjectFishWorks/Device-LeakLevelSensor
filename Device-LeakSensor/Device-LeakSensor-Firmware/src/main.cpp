49c391652f053b44534add522b4d2d14970de3bc
Kayleb814 <kayleb_s@hotmail.com>
2024-10-20 18:41:23 -0700

    Testing code working but motor driver not and leak sensor is not consistent

--- Device-LeakSensor/Device-LeakSensor-Firmware/src/main.cpp
+++ Device-LeakSensor/Device-LeakSensor-Firmware/src/main.cpp
@@ -13,12 +13,13 @@
 #define PUMP_IN1_PIN 8
 #define PUMP_IN2_PIN 9
 #define PUMP_PWM_PIN 5
+int levelSwitchStatus;
+int leakSensorStatus;
+int minPumpSpeed = 50;
+int maxPumpSpeed = 155;
 
 #define sendMessageDelay 1000
-#define MessageGap 5000
-
-int pumpEnabled = (PUMP_IN1_PIN == HIGH) && (PUMP_IN2_PIN == LOW);
-int pumpDisabled = (PUMP_IN1_PIN == LOW) && (PUMP_IN2_PIN == LOW);
+#define MessageGap 2000
 
 // Node controller core object
 NodeControllerCore core;
@@ -28,24 +29,169 @@ NodeControllerCore core;
 // Function to send message
 void send_message(uint8_t nodeID, uint16_t messageID, uint64_t data);
 
-// Function to check the leak sensor
+// Function to enable the pump
+void pumpEnabled();
+
+// Function to disable the pump
+void pumpDisabled();
+
+// Function to check the tank level sensor
+void chkTankLevelSwitch();
+
+// Function to chk the leak sensor
 void chkLeakSensor();
 
-// Function to check the level switch
-void chkLevelSwitch();
-
-void pumpControl()
+void setup()
 {
-  pumpEnabled;
-  serial.println("Pump is enabled");
-  serial.println(pumpEnabled);
-  if (digitalRead(LEVEL_SWITCH_PIN) == LOW)
+  // put your setup code here, to run once:
+
+  // Initialize serial communication
+  Serial.begin(115200);
+
+  pinMode(LEVEL_SWITCH_PIN, INPUT_PULLUP);
+  pinMode(LEAK_SENSOR_PIN, INPUT);
+  pinMode(PUMP_IN1_PIN, OUTPUT);
+  pinMode(PUMP_IN2_PIN, OUTPUT);
+  pinMode(PUMP_PWM_PIN, OUTPUT);
+
+  // Create the node controller core object
+  core = NodeControllerCore();
+  Serial.println("test");
+  // Initialize the node controller core object
+  if (core.Init(send_message, NODE_ID))
   {
-    analogWrite(PUMP_PWM_PIN, 255);
+    Serial.println("Driver device initialized");
   }
   else
   {
+    Serial.println("Failed to initialize driver");
+  }
+  delay(sendMessageDelay);
+  /*
+  xTaskCreate(chkLeakSensor,
+              "chkLeakSensor",
+              10000,
+              NULL,
+              1,
+              NULL
+              );
+  */
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+  chkLeakSensor();
+  chkTankLevelSwitch();
+  delay(MessageGap);
+}
+
+// put function definitions here:
+
+void send_message(uint8_t nodeID, uint16_t messageID, uint64_t data)
+{
+
+  Serial.print("Received message with ID: ");
+  Serial.println(messageID, HEX);
+  Serial.print("Data: ");
+  for (int i = 0; i < data; i++)
+  {
+    Serial.print(data);
+    Serial.println(" ");
+  }
+  Serial.println();
+}
+
+void pumpEnabled()
+{
+  digitalWrite(PUMP_IN1_PIN, HIGH);
+  digitalWrite(PUMP_IN2_PIN, LOW);
+}
+
+void pumpDisabled()
+{
+  digitalWrite(PUMP_IN1_PIN, LOW);
+  digitalWrite(PUMP_IN2_PIN, LOW);
+}
+
+// Function to check the tank level sensor
+void chkTankLevelSwitch()
+{
+  levelSwitchStatus = digitalRead(LEVEL_SWITCH_PIN);
+  leakSensorStatus = analogReadMilliVolts(LEAK_SENSOR_PIN);
+  if (levelSwitchStatus == 0 && leakSensorStatus > 50)
+  {
+    analogWrite(PUMP_PWM_PIN, minPumpSpeed);
+
+#ifdef debuging
+    Serial.println("Tank is High");
+    Serial.println("Pump is disabled");
+    Serial.print("pumpIN1   ");
+    Serial.print(digitalRead(PUMP_IN1_PIN));
+    Serial.print("    pumpIN2  ");
+    Serial.println(digitalRead(PUMP_IN2_PIN));
+    Serial.print("Leak Sensor =  ");
+    Serial.println(leakSensorStatus);
+    Serial.println("");
+#endif
+  }
+  else if(levelSwitchStatus == 1 && leakSensorStatus > 50)
+  {
+    pumpEnabled();
+    analogWrite(PUMP_PWM_PIN, maxPumpSpeed);
+
+#ifdef debuging
+    Serial.println("Tank level is normal");
+    Serial.println("Pump is enabled");
+    Serial.print("pumpIN1   ");
+    Serial.print(digitalRead(PUMP_IN1_PIN));
+    Serial.print("    pumpIN2   ");
+    Serial.println(digitalRead(PUMP_IN2_PIN));
+    Serial.print("Leak Sensor =  ");
+    Serial.println(leakSensorStatus);
+    Serial.println("");
+#endif
+  }
+}
+
+// Function to chk the leak sensor
+void chkLeakSensor()
+{
+  leakSensorStatus = analogReadMilliVolts(LEAK_SENSOR_PIN);
+  if (leakSensorStatus < 50)
+  {
+    pumpDisabled();
     analogWrite(PUMP_PWM_PIN, 0);
+
+#ifdef debuging
+    Serial.println("Leak detected");
+    Serial.println("Pump is disabled");
+    Serial.print("pumpIN1   ");
+    Serial.print(digitalRead(PUMP_IN1_PIN));
+    Serial.print("    pumpIN2  ");
+    Serial.println(digitalRead(PUMP_IN2_PIN));
+    Serial.print("Leak Sensor =  ");
+    Serial.println(leakSensorStatus);
+    Serial.println("");
+#endif
+  }
+  else
+  {
+    pumpEnabled();
+    analogWrite(PUMP_PWM_PIN, maxPumpSpeed);
+
+#ifdef debuging
+    Serial.println("No leaks detected");
+    Serial.println("Pump is enabled");
+    Serial.print("pumpIN1   ");
+    Serial.print(digitalRead(PUMP_IN1_PIN));
+    Serial.print("    pumpIN2   ");
+    Serial.println(digitalRead(PUMP_IN2_PIN));
+    Serial.print("Leak Sensor =  ");
+    Serial.println(leakSensorStatus);
+    Serial.println("");
+#endif
   }
 }
 /*
@@ -83,107 +229,3 @@ void SendTempHumMessage(void *parameters)
   }
 }
 */
-
-void setup()
-{
-  // put your setup code here, to run once:
-
-  // Initialize serial communication
-  Serial.begin(115200);
-
-  pinMode(LEVEL_SWITCH_PIN, INPUT_PULLUP);
-  pinMode(LEAK_SENSOR_PIN, INPUT);
-
-  // Create the node controller core object
-  core = NodeControllerCore();
-  Serial.println("test");
-  // Initialize the node controller core object
-  if (core.Init(send_message, NODE_ID))
-  {
-    Serial.println("Driver device initialized");
-  }
-  else
-  {
-    Serial.println("Failed to initialize driver");
-  }
-  delay(sendMessageDelay);
-  /*
-  xTaskCreate(chkLeakSensor,
-              "chkLeakSensor",
-              10000,
-              NULL,
-              1,
-              NULL
-              );
-  */
-}
-
-void loop()
-{
-  // put your main code here, to run repeatedly:
-  Serial.println("Sending message");
-  chkLeakSensor();
-  chkLevelSwitch();
-  delay(MessageGap);
-  Serial.println("Sending message");
-}
-
-// put function definitions here:
-
-void send_message(uint8_t nodeID, uint16_t messageID, uint64_t data)
-{
-
-  Serial.print("Received message with ID: ");
-  Serial.println(messageID, HEX);
-  Serial.print("Data: ");
-  for (int i = 0; i < data; i++)
-  {
-    Serial.print(data);
-    Serial.println(" ");
-  }
-  Serial.println();
-}
-
-// Function to check the leak sensor
-void chkLeakSensor()
-{
-  Serial.println("Checking Leak Sensor");
-  int sensorValue = analogReadMilliVolts(LEAK_SENSOR_PIN);
-
-  Serial.println(sensorValue);
-  if (sensorValue > 1)
-  {
-#ifdef debuging
-    Serial.println("Leak Detected");
-#endif
-  }
-  else
-  {
-#ifdef debuging
-    Serial.println("No Leak Detected");
-#endif
-  }
-
-  if ()
-
-    delay(sendMessageDelay);
-}
-
-// Function to check the level switch
-void chkLevelSwitch()
-{
-  int levelSwitchValue = digitalRead(LEVEL_SWITCH_PIN);
-  if (levelSwitchValue == LOW)
-  {
-#ifdef debuging
-    Serial.println("Level Switch is triggered");
-#endif
-  }
-  else
-  {
-#ifdef debuging
-    Serial.println("Level Switch is not triggered");
-#endif
-  }
-  delay(sendMessageDelay);
-}
