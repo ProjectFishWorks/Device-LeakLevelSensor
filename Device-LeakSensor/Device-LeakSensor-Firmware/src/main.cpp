f6ddfa9fdc9e2d17703de8c32d3addf4bedc830e
sebastien <sebastien@robitaille.info>
2024-11-01 15:15:51 -0700

    Add main functionality

--- Device-LeakSensor/Device-LeakSensor-Firmware/src/main.cpp
+++ Device-LeakSensor/Device-LeakSensor-Firmware/src/main.cpp
@@ -2,26 +2,62 @@
 #include <NodeControllerCore.h>
 
 #define NODE_ID 0xB2                       // 178
-#define LEAK_DETECTED_MESSAGE_ID 0x0A00    // 2560
-#define HIGH_WATER_LEVEL_MESSAGE_ID 0x0A01 // 2561
-#define PUMP_MAX_SPEED_MESSAGE_ID 0x0A02   // 2562
-#define PUMP_MIN_SPEED_MESSAGE_ID 0x0A03   // 2563
-#define ALARM_MESSAGE_ID 901               //
+#define LEAK_DETECTED_1_MESSAGE_ID 0x0A01    // 2561
+#define LEAK_DETECTED_2_MESSAGE_ID 0x0A02    // 2562
+#define LEAK_DETECTED_3_MESSAGE_ID 0x0A03    // 2563
+#define LEAK_DETECTED_4_MESSAGE_ID 0x0A04    // 2564
+
+#define HIGH_WATER_1_LEVEL_MESSAGE_ID 0x0B01 // 2817
+#define HIGH_WATER_2_LEVEL_MESSAGE_ID 0x0B02 // 2818
+#define HIGH_WATER_3_LEVEL_MESSAGE_ID 0x0B03 // 2819
+#define HIGH_WATER_4_LEVEL_MESSAGE_ID 0x0B04 // 2820
+
+#define ALARM_MESSAGE_ID 901               
 
 #define debuging
 
-#define LEAK_SENSOR_PIN 4
-#define LEVEL_SWITCH_PIN 10
-#define PUMP_IN1_PIN 8
-#define PUMP_IN2_PIN 9
-#define PUMP_PWM_PIN 5
-int levelSwitchStatus;
-int leakSensorStatus;
-int minPumpSpeed = 50;
-int maxPumpSpeed = 155;
+#define LEAK_SENSOR_PIN_1 0
+#define LEAK_SENSOR_PIN_2 1
+#define LEAK_SENSOR_PIN_3 2
+#define LEAK_SENSOR_PIN_4 3
 
-#define sendMessageDelay 1000
-#define MessageGap 2000
+#define LEVEL_SWITCH_PIN_1 4
+#define LEVEL_SWITCH_PIN_2 5
+#define LEVEL_SWITCH_PIN_3 8
+#define LEVEL_SWITCH_PIN_4 9
+
+#define LEAK_SENSOR_THRESHOLD 150
+
+int levelSwitchStatus;
+
+#define updateInterval 2000
+
+uint8_t hasSentHighWaterLevelAlarm1 = 0;
+uint8_t hasSentHighWaterLevelAlarm2 = 0;
+uint8_t hasSentHighWaterLevelAlarm3 = 0;
+uint8_t hasSentHighWaterLevelAlarm4 = 0;
+
+uint8_t hasSentLeak1Alarm = 0;
+uint8_t hasSentLeak2Alarm = 0;
+uint8_t hasSentLeak3Alarm = 0;
+uint8_t hasSentLeak4Alarm = 0;
+
+
+uint8_t hasSentNoLeakAlarm1 = 0;
+uint8_t hasSentNoLeakAlarm2 = 0;
+uint8_t hasSentNoLeakAlarm3 = 0;
+uint8_t hasSentNoLeakAlarm4 = 0;
+
+
+uint8_t hasSentNoHighWaterLevelAlarm1 = 0;
+uint8_t hasSentNoHighWaterLevelAlarm2 = 0;
+uint8_t hasSentNoHighWaterLevelAlarm3 = 0;
+uint8_t hasSentNoHighWaterLevelAlarm4 = 0;
+
+uint8_t hasSentGeneralAlarm = 0;
+uint8_t hasSentNoGeneralAlarm = 0;
+
+
 
 // Node controller core object
 NodeControllerCore core;
@@ -29,19 +65,17 @@ NodeControllerCore core;
 // put function declarations here:
 
 // Function to send message
-void send_message(uint8_t nodeID, uint16_t messageID, uint64_t data);
-
-// Function to enable the pump
-void pumpEnabled();
-
-// Function to disable the pump
-void pumpDisabled();
+void messageReceived(uint8_t nodeID, uint16_t messageID, uint64_t data);
 
 // Function to check the tank level sensor
-void chkTankLevelSwitch();
+void checkForGlobalAlerts(void *parameters);
+
+void checkForLevelAlerts(void *parameters);
+
+void checkForLeakAlerts(void *parameters);
 
 // Function to chk the leak sensor
-//void chkLeakSensor();
+void checkLeakSensor();
 
 void setup()
 {
@@ -50,17 +84,21 @@ void setup()
   // Initialize serial communication
   Serial.begin(115200);
 
-  pinMode(LEVEL_SWITCH_PIN, INPUT_PULLUP);
-  pinMode(LEAK_SENSOR_PIN, INPUT);
-  pinMode(PUMP_IN1_PIN, OUTPUT);
-  pinMode(PUMP_IN2_PIN, OUTPUT);
-  pinMode(PUMP_PWM_PIN, OUTPUT);
+  pinMode(LEVEL_SWITCH_PIN_1, INPUT_PULLUP);
+  pinMode(LEVEL_SWITCH_PIN_2, INPUT_PULLUP);
+  pinMode(LEVEL_SWITCH_PIN_3, INPUT_PULLUP);
+  pinMode(LEVEL_SWITCH_PIN_4, INPUT_PULLUP);
+
+  pinMode(LEAK_SENSOR_PIN_1, INPUT);
+  pinMode(LEAK_SENSOR_PIN_2, INPUT);
+  pinMode(LEAK_SENSOR_PIN_3, INPUT);
+  pinMode(LEAK_SENSOR_PIN_4, INPUT);
 
   // Create the node controller core object
   core = NodeControllerCore();
   Serial.println("test");
   // Initialize the node controller core object
-  if (core.Init(send_message, NODE_ID))
+  if (core.Init(messageReceived, NODE_ID))
   {
     Serial.println("Driver device initialized");
   }
@@ -68,136 +106,374 @@ void setup()
   {
     Serial.println("Failed to initialize driver");
   }
-  delay(sendMessageDelay);
-  /*
-  xTaskCreate(chkLeakSensor,
-              "chkLeakSensor",
-              10000,
-              NULL,
-              1,
-              NULL
-              );
-  */
+
+  xTaskCreatePinnedToCore(
+    checkForGlobalAlerts, /* Function to implement the task */
+    "checkForGlobalAlerts", /* Name of the task */
+    10000,                  /* Stack size in words */
+    NULL,                   /* Task input parameter */
+    1,                      /* Priority of the task */
+    NULL,                   /* Task handle. */
+    1);
+  xTaskCreatePinnedToCore(
+      checkForLevelAlerts, /* Function to implement the task */
+      "checkForAlerts", /* Name of the task */
+      10000,                  /* Stack size in words */
+      NULL,                   /* Task input parameter */
+      1,                      /* Priority of the task */
+      NULL,                   /* Task handle. */
+      1);
+  xTaskCreatePinnedToCore(
+      checkForLeakAlerts, /* Function to implement the task */
+      "checkForLeakAlerts", /* Name of the task */
+      10000,                  /* Stack size in words */
+      NULL,                   /* Task input parameter */
+      1,                      /* Priority of the task */
+      NULL,                   /* Task handle. */
+      1);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
 
-  chkLeakSensor();
-  chkTankLevelSwitch();
-  //delay(MessageGap);
-  //digitalWrite(PUMP_IN1_PIN, 1);
-  //digitalWrite(PUMP_IN2_PIN, 1);
 }
 
 // put function definitions here:
 
-void send_message(uint8_t nodeID, uint16_t messageID, uint64_t data)
+void messageReceived(uint8_t nodeID, uint16_t messageID, uint64_t data)
 {
 
   Serial.print("Received message with ID: ");
   Serial.println(messageID, HEX);
   Serial.print("Data: ");
-  for (int i = 0; i < data; i++)
-  {
-    Serial.print(data);
-    Serial.println(" ");
-  }
+
   Serial.println();
 }
 
-void pumpEnabled()
-{
-  digitalWrite(PUMP_IN1_PIN, HIGH);
-  digitalWrite(PUMP_IN2_PIN, LOW);
-}
-
-void pumpDisabled()
-{
-  digitalWrite(PUMP_IN1_PIN, LOW);
-  digitalWrite(PUMP_IN2_PIN, LOW);
-}
-
 // Function to check the tank level sensor
-void chkTankLevelSwitch()
+void checkForGlobalAlerts(void *parameters)
 {
-  levelSwitchStatus = digitalRead(LEVEL_SWITCH_PIN);
-  leakSensorStatus = analogReadMilliVolts(LEAK_SENSOR_PIN);
-  if (levelSwitchStatus == 1 )//&& leakSensorStatus > 150)
-  {
-    pumpEnabled();
-    analogWrite(PUMP_PWM_PIN, minPumpSpeed);
+  Serial.println("Checking Tank Level Switch Task");
 
-#ifdef debuging
-    Serial.println("Tank is High");
-    Serial.println("Pump is reduced speed");
-    Serial.print("pumpIN1   ");
-    //Serial.print(digitalRead(PUMP_IN1_PIN));
-    Serial.print("    pumpIN2  ");
-    //Serial.println(digitalRead(PUMP_IN2_PIN));
-    Serial.print("Leak Sensor =  ");
-    Serial.println(leakSensorStatus);
-    Serial.println("");
-#endif
-  }
-  else if(levelSwitchStatus == 1 )//&& leakSensorStatus > 150)
-  {
-    pumpEnabled();
-    analogWrite(PUMP_PWM_PIN, maxPumpSpeed);
+  uint64_t generalErrorData1 = 0x1;
+  uint64_t generalErrorData0 = 0x0;
 
-#ifdef debuging
-    Serial.println("Tank level is normal");
-    Serial.println("Pump is enabled");
-    Serial.print("pumpIN1   ");
-    //Serial.print(digitalRead(PUMP_IN1_PIN));
-    Serial.print("    pumpIN2   ");
-    //Serial.println(digitalRead(PUMP_IN2_PIN));
-    Serial.print("Leak Sensor =  ");
-    Serial.println(leakSensorStatus);
-    Serial.println("");
-#endif
+  while(1){
+    uint8_t  levelSwitchStatus1 = (digitalRead(LEVEL_SWITCH_PIN_1));
+    uint8_t  levelSwitchStatus2 = (digitalRead(LEVEL_SWITCH_PIN_2));
+    uint8_t  levelSwitchStatus3 = (digitalRead(LEVEL_SWITCH_PIN_3));
+    uint8_t  levelSwitchStatus4 = (digitalRead(LEVEL_SWITCH_PIN_4));
+
+    int leakSensorStatus1 = analogReadMilliVolts(LEAK_SENSOR_PIN_1);
+    Serial.println("Leak Sensor 1 = " + String(leakSensorStatus1));
+    int leakSensorStatus2 = analogReadMilliVolts(LEAK_SENSOR_PIN_2);
+    Serial.println("Leak Sensor 2 = " + String(leakSensorStatus2));
+    int leakSensorStatus3 = analogReadMilliVolts(LEAK_SENSOR_PIN_3);
+    Serial.println("Leak Sensor 3 = " + String(leakSensorStatus3));
+    int leakSensorStatus4 = analogReadMilliVolts(LEAK_SENSOR_PIN_4);
+    Serial.println("Leak Sensor 4 = " + String(leakSensorStatus4));
+
+    if (leakSensorStatus1 > LEAK_SENSOR_THRESHOLD || 
+        leakSensorStatus2 > LEAK_SENSOR_THRESHOLD || 
+        leakSensorStatus3 > LEAK_SENSOR_THRESHOLD || 
+        leakSensorStatus4 > LEAK_SENSOR_THRESHOLD ||
+        levelSwitchStatus1 == 0 ||
+        levelSwitchStatus2 == 0 ||
+        levelSwitchStatus3 == 0 ||
+        levelSwitchStatus4 == 0)
+    {
+      if(hasSentGeneralAlarm == 0)
+      {
+        core.sendMessage(ALARM_MESSAGE_ID, &generalErrorData0);
+        core.sendMessage(ALARM_MESSAGE_ID, &generalErrorData1);
+        hasSentGeneralAlarm = 1;
+        hasSentNoGeneralAlarm = 0;
+            #ifdef debuging
+          Serial.println("General Alarm sent!!!!");
+        #endif
+      }else{
+            #ifdef debuging
+          Serial.println("General Alarm Not sent");
+      #endif
+      }
+    }
+    else
+    {
+      if(hasSentNoGeneralAlarm == 0)
+      {
+        core.sendMessage(ALARM_MESSAGE_ID, &generalErrorData0);
+        hasSentGeneralAlarm = 0;
+        hasSentNoGeneralAlarm = 1;
+            #ifdef debuging
+          Serial.println("No General Alarm not sent");
+      #endif
+      }
+      else{
+            #ifdef debuging
+          Serial.println("No General Alarm not sent");
+      #endif
+      }
+
+    }
+    delay(updateInterval);
   }
+
+}
+
+void checkForLevelAlerts(void *parameters)
+{
+
+  uint64_t errorData1 = 0x1;
+  uint64_t errorData0 = 0x0;
+
+  Serial.println("Checking Tank Level Switch Task");
+
+  while(1){
+    uint8_t  levelSwitchStatus1 = (digitalRead(LEVEL_SWITCH_PIN_1));
+    uint8_t  levelSwitchStatus2 = (digitalRead(LEVEL_SWITCH_PIN_2));
+    uint8_t  levelSwitchStatus3 = (digitalRead(LEVEL_SWITCH_PIN_3));
+    uint8_t  levelSwitchStatus4 = (digitalRead(LEVEL_SWITCH_PIN_4));
+
+    if (levelSwitchStatus1 == 0)
+    {
+      if(hasSentHighWaterLevelAlarm1 == 0)
+      {
+        //core.sendMessage(HIGH_WATER_1_LEVEL_MESSAGE_ID, (uint64_t)0);
+        core.sendMessage(HIGH_WATER_1_LEVEL_MESSAGE_ID, &errorData1);
+        hasSentHighWaterLevelAlarm1 = 1;
+        hasSentNoHighWaterLevelAlarm1 = 0;
+      }
+      #ifdef debuging
+        Serial.println("Tank 1 is High");
+      #endif
+    }
+    else
+    {
+      if(hasSentNoHighWaterLevelAlarm1 == 0)
+      {
+        core.sendMessage(HIGH_WATER_1_LEVEL_MESSAGE_ID, (uint64_t)0);
+        hasSentHighWaterLevelAlarm1 = 0;
+        hasSentNoHighWaterLevelAlarm1 = 1;
+      }
+      #ifdef debuging
+        Serial.println("Tank 1 level is normal");
+      #endif
+    }
+
+    if (levelSwitchStatus2 == 0)
+    {
+      if(hasSentHighWaterLevelAlarm2 == 0)
+      {
+        //core.sendMessage(HIGH_WATER_2_LEVEL_MESSAGE_ID, (uint64_t)0);
+        core.sendMessage(HIGH_WATER_2_LEVEL_MESSAGE_ID, &errorData1);
+        hasSentHighWaterLevelAlarm2 = 1;
+        hasSentNoHighWaterLevelAlarm2 = 0;
+      }
+      #ifdef debuging
+        Serial.println("Tank 2 is High");
+      #endif
+    }
+    else
+    {
+      if(hasSentNoHighWaterLevelAlarm2 == 0)
+      {
+        core.sendMessage(HIGH_WATER_2_LEVEL_MESSAGE_ID, (uint64_t)0);
+        hasSentHighWaterLevelAlarm2 = 0;
+        hasSentNoHighWaterLevelAlarm2 = 1;
+      }
+      #ifdef debuging
+        Serial.println("Tank 2 level is normal");
+      #endif
+    }
+
+    if (levelSwitchStatus3 == 0)
+    {
+      if(hasSentHighWaterLevelAlarm3 == 0)
+      {
+        //core.sendMessage(HIGH_WATER_3_LEVEL_MESSAGE_ID, (uint64_t)0);
+        core.sendMessage(HIGH_WATER_3_LEVEL_MESSAGE_ID, &errorData1);
+        hasSentHighWaterLevelAlarm3 = 1;
+        hasSentNoHighWaterLevelAlarm3 = 0;
+      }
+      #ifdef debuging
+        Serial.println("Tank 3 is High");
+      #endif
+    }
+    else
+    {
+      if(hasSentNoHighWaterLevelAlarm3 == 0)
+      {
+        core.sendMessage(HIGH_WATER_3_LEVEL_MESSAGE_ID, (uint64_t)0);
+        hasSentHighWaterLevelAlarm3 = 0;
+        hasSentNoHighWaterLevelAlarm3 = 1;
+      }
+      #ifdef debuging
+        Serial.println("Tank 3 level is normal");
+      #endif
+    }
+
+    if (levelSwitchStatus4 == 0)
+    {
+      if(hasSentHighWaterLevelAlarm4  == 0)
+      {
+        //core.sendMessage(HIGH_WATER_4_LEVEL_MESSAGE_ID, (uint64_t)0);
+        core.sendMessage(HIGH_WATER_4_LEVEL_MESSAGE_ID, &errorData1);
+        hasSentHighWaterLevelAlarm4 = 1;
+        hasSentNoHighWaterLevelAlarm4 = 0;
+      }
+      #ifdef debuging
+        Serial.println("Tank 4 is High");
+      #endif
+    }
+    else
+    {
+      if(hasSentNoHighWaterLevelAlarm4 == 0)
+      {
+        core.sendMessage(HIGH_WATER_4_LEVEL_MESSAGE_ID, (uint64_t)0);
+        hasSentHighWaterLevelAlarm4 = 0;
+        hasSentNoHighWaterLevelAlarm4 = 1;
+      }
+      #ifdef debuging
+        Serial.println("Tank 4 level is normal");
+      #endif
+    }
+    
+    delay(updateInterval);
+  }
+
+}
+
+void checkForLeakAlerts(void *parameters){
+
+    uint64_t errorData1 = 0x1;
+    uint64_t errorData0 = 0x0;
+
+    while(1){
+      
+      int leakSensorStatus1 = analogReadMilliVolts(LEAK_SENSOR_PIN_1);
+      Serial.println("Leak Sensor 1 = " + String(leakSensorStatus1));
+      int leakSensorStatus2 = analogReadMilliVolts(LEAK_SENSOR_PIN_2);
+      Serial.println("Leak Sensor 2 = " + String(leakSensorStatus2));
+      int leakSensorStatus3 = analogReadMilliVolts(LEAK_SENSOR_PIN_3);
+      Serial.println("Leak Sensor 3 = " + String(leakSensorStatus3));
+      int leakSensorStatus4 = analogReadMilliVolts(LEAK_SENSOR_PIN_4);
+      Serial.println("Leak Sensor 4 = " + String(leakSensorStatus4));
+
+      if (leakSensorStatus1 > LEAK_SENSOR_THRESHOLD)
+    {
+      if(hasSentLeak1Alarm == 0)
+      {
+        //core.sendMessage(LEAK_DETECTED_1_MESSAGE_ID, (uint64_t)0);
+        core.sendMessage(LEAK_DETECTED_1_MESSAGE_ID, &errorData1);
+        hasSentLeak1Alarm = 1;
+        hasSentNoLeakAlarm1 = 0;
+      }
+      #ifdef debuging
+          Serial.println("Leak 1 detected");
+      #endif
+    }
+    else
+    {
+      if(hasSentNoLeakAlarm1 == 0)
+      {
+        core.sendMessage(LEAK_DETECTED_1_MESSAGE_ID, (uint64_t)0);
+        hasSentLeak1Alarm = 0;
+        hasSentNoLeakAlarm1 = 1;
+      }
+      #ifdef debuging
+          Serial.println("No leak 1 detected");
+      #endif
+    }
+
+    if (leakSensorStatus2 > LEAK_SENSOR_THRESHOLD)
+    {
+      if(hasSentLeak2Alarm == 0)
+      {
+        //core.sendMessage(LEAK_DETECTED_2_MESSAGE_ID, (uint64_t)0);
+        core.sendMessage(LEAK_DETECTED_2_MESSAGE_ID, &errorData1);
+        hasSentLeak2Alarm = 1;
+        hasSentNoLeakAlarm2 = 0;
+      }
+      #ifdef debuging
+          Serial.println("Leak 2 detected");
+      #endif
+    }
+    else
+    {
+      if(hasSentNoLeakAlarm2 == 0)
+      {
+        core.sendMessage(LEAK_DETECTED_2_MESSAGE_ID, (uint64_t)0);
+        hasSentLeak2Alarm = 0;
+        hasSentNoLeakAlarm2 = 1;
+      }
+      #ifdef debuging
+          Serial.println("No leak 2 detected");
+      #endif
+    }
+
+    if (leakSensorStatus3 > LEAK_SENSOR_THRESHOLD)
+    {
+      if(hasSentLeak3Alarm == 0)
+      {
+        //core.sendMessage(LEAK_DETECTED_3_MESSAGE_ID, (uint64_t)0);
+        core.sendMessage(LEAK_DETECTED_3_MESSAGE_ID, &errorData1);
+        hasSentLeak3Alarm = 1;
+        hasSentNoLeakAlarm3 = 0;
+      }
+      #ifdef debuging
+          Serial.println("Leak 3 detected");
+      #endif
+    }
+    else
+    {
+      if(hasSentNoLeakAlarm3 == 0)
+      {
+        core.sendMessage(LEAK_DETECTED_3_MESSAGE_ID, (uint64_t)0);
+        hasSentLeak3Alarm = 0;
+        hasSentNoLeakAlarm3 = 1;
+      }
+      #ifdef debuging
+          Serial.println("No leak 3 detected");
+      #endif
+    }
+
+    if (leakSensorStatus4 > LEAK_SENSOR_THRESHOLD)
+    {
+      if(hasSentLeak4Alarm == 0)
+      {
+        //core.sendMessage(LEAK_DETECTED_3_MESSAGE_ID, (uint64_t)0);
+        core.sendMessage(LEAK_DETECTED_4_MESSAGE_ID, &errorData1);
+        hasSentLeak4Alarm = 1;
+        hasSentNoLeakAlarm4 = 0;
+      }
+      #ifdef debuging
+          Serial.println("Leak 4 detected");
+      #endif
+    }
+    else
+    {
+      if(hasSentNoLeakAlarm4 == 0)
+      {
+        core.sendMessage(LEAK_DETECTED_4_MESSAGE_ID, (uint64_t)0);
+        hasSentLeak4Alarm = 0;
+        hasSentNoLeakAlarm4 = 1;
+      }
+      #ifdef debuging
+          Serial.println("No leak 4 detected");
+      #endif
+    }
+    delay(updateInterval);
+    }
+
+      
 }
 
 // Function to chk the leak sensor
-void chkLeakSensor()
+void checkLeakSensor()
 {
-  leakSensorStatus = analogReadMilliVolts(LEAK_SENSOR_PIN);
-  if (leakSensorStatus > 150)
-  {
-    pumpDisabled();
-    analogWrite(PUMP_PWM_PIN, 0);
+  uint64_t errorData1 = 0x1;
+  uint64_t errorData0 = 0x0;
 
-#ifdef debuging
-    Serial.println("Leak detected");
-    Serial.println("Pump is disabled");
-    Serial.print("pumpIN1   ");
-    //Serial.print(digitalRead(PUMP_IN1_PIN));
-    Serial.print("    pumpIN2  ");
-    //Serial.println(digitalRead(PUMP_IN2_PIN));
-    Serial.print("Leak Sensor =  ");
-    Serial.println(leakSensorStatus);
-    Serial.println("");
-#endif
-  }
-  else
-  {
-    pumpEnabled();
-    analogWrite(PUMP_PWM_PIN, maxPumpSpeed);
-
-#ifdef debuging
-    Serial.println("No leaks detected");
-    Serial.println("Pump is enabled");
-    Serial.print("pumpIN1   ");
-    //Serial.print(digitalRead(PUMP_IN1_PIN));
-    Serial.print("    pumpIN2   ");
-    //Serial.println(digitalRead(PUMP_IN2_PIN));
-    Serial.print("Leak Sensor =  ");
-    Serial.println(leakSensorStatus);
-    Serial.println("");
-#endif
-  }
 }
 /*
 // Function to send message
